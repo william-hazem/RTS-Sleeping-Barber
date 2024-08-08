41cb62a3e4c4388a724febbe48b33b5450794411
William Henrique <william.martins@ee.ufcg.edu.br>
2024-08-08 09:25:38 -0300

    update

--- main.cpp
+++ main.cpp
@@ -8,12 +8,21 @@
 #include <inttypes.h>
 #include <queue>
 
+// Problemas divertidos de experiência:
+//  waiting for a resource infinitly because someone hold it and finished without release the resource
+//
+
 #define MAX 5
-std::queue<int> q_wait;              // FIFO de espera
+#define CLIENT_RATE 40               // client arrive rate
+std::queue<int> q_wait;              // waiting FIFO
+int wait_chairs;
 //pthread_mutex_t barbeiros[3];      // Barbeiros - dormindo/acordado  
-sem_t s_barbeiros[3];
+sem_t s_barbers[3];                  // sleeping=0/waked=1
+sem_t s_queue;
 
 #define T 1                          // Time constant
+
+void *f_client(void *arg);
 
 void *f_barbershop(void *arg)
 {   
@@ -22,19 +31,50 @@ void *f_barbershop(void *arg)
     for(;;)
     {
         int r = rand() % 100;
-        if(r >= 80)
+        if(r >= 100 - CLIENT_RATE)
         {
-            // logger(LOG_DEBUG, "Client arrived");
-            if(q_wait.size() < MAX)
-            {
-                logger(LOG_INFO, "Client waiting (%d)", client_counter);
-                q_wait.push(client_counter);
-                client_counter++;
-            } else
-                logger(LOG_WARNING, "No chairs to wait");
-            
+            pthread_t tid;
+            logger(LOG_DEBUG, "Client %d arrived", client_counter);
+            uint32_t temp = client_counter;
+            if(pthread_create(&tid, NULL, f_client, (void*)&temp) != 0)
+                logger(LOG_ERROR, "Client thread not started");
+            client_counter++;   
+        }
+        sleep(T);
+    }
+    return NULL;
+}
+
+void *f_client(void *arg)
+{
+    uint32_t client_id = *(uint32_t*)arg;
+    for(;;)
+    {
+        
+        if(q_wait.size() >= MAX)
+        {
+            logger(LOG_WARNING, "FULL QUEUE! - Client %d leaves", client_id);
+            return NULL;
         }
 
+        sem_wait(&s_queue);
+        q_wait.push(client_id);
+        logger(LOG_INFO, "CLient %d is waiting (%d/%d)", client_id, q_wait.size(), MAX);
+        sem_post(&s_queue);
+
+        /// PRECISA MELHORAR
+        for(int i = 0; i < 3; i++)
+        {
+            int value;
+            sem_getvalue(&s_barbers[i], &value); 
+            // if(i == 2) logger(LOG_ERROR, "Barber v=%d", value);
+            if(value == 0) { // barbers i is sleeping
+                sem_post(&s_barbers[i]);    
+                break;
+            }
+        }
+        break;
+        
         sleep(T);
     }
     return NULL;
@@ -42,23 +82,31 @@ void *f_barbershop(void *arg)
 
 void *f_barber(void *arg)
 {
-    static int id = 0;
-    logger(LOG_INFO, "Barber %d created with pid %lu", id++, (unsigned long)pthread_self());
+
+    int id = *(int*) arg;
+    logger(LOG_INFO, "Barber %d created with pid %lu", id, (unsigned long)pthread_self());
     for(;;)
     {
+        sem_wait(&s_barbers[id]);
+        logger(LOG_INFO, "Barber %d waked", id);
+        for(;;)
+        {
+            
+            sem_wait(&s_queue);             // critical section to get client
+            if(q_wait.size() == 0) break;   // no client waiting
+            int client_id = q_wait.front();
+            q_wait.pop();
+            sem_post(&s_queue);
 
-        sleep(T);
+            logger(LOG_DEBUG, "Barber %d cutting client %d hair", id, client_id);
+            // cut client hair
+            sleep(10*T);
+            logger(LOG_DEBUG, "Barber %d ENDED client %d hair", id, client_id);
+
+        }
+        logger(LOG_INFO, "Barber %d going to sleep", id);
+        
     }
-    printf("Thread: Hello World! \n");
-    return NULL;
-}
-
-void *thread_function2(void *arg)
-{
-    clock_t c = clock();
-    sleep(1);
-    float seconds = (float)(clock() - c) / CLOCKS_PER_SEC;
-    printf("Thread: %lf\n", seconds);
     return NULL;
 }
 
@@ -68,13 +116,20 @@ int main()
     pthread_t thread1;
     pthread_t thread2;
     pthread_t t_baber[3];
-
-    pthread_create(&thread1, NULL, f_barbershop, NULL);
+    
+    sem_init(&s_queue, 0, 1);
+    uint32_t barber_ids[] = {0, 1, 2};
     for(int i = 0; i < 3; i++)
-        if(pthread_create(&t_baber[i], NULL, f_barber, NULL) != 0)
+    {
+        sem_init(&s_barbers[i], 0, 0);
+        if(pthread_create(&t_baber[i], NULL, f_barber, &barber_ids[i]) != 0)
+        {
             logger(LOG_ERROR, "Error when creating barber tasks");
-
-    pthread_create(&thread2, NULL, thread_function2, NULL);
+            exit(1);
+        }
+    }
+    sleep(1);
+    pthread_create(&thread1, NULL, f_barbershop, NULL);
 
     pthread_exit(NULL);
     return 0; 
